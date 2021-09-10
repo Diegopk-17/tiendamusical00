d2bec17faf535144f6c2634f36e3b3e5ec69af70
Diegopk-17 <diegopk-fx@hotmail.com>
2021-09-09 19:19:57 -0500

    Nombre del proyecto: Tienda Musical
    Descripcion: se agrega la modificacion de las secciones de pantalla
    footer y header, así como la creacion e implementacion de la pantalla
    detalle y el uso del badge.

--- tm-web/src/main/java/com/devpredator/tiendamusicalweb/controllers/HomeController.java
+++ tm-web/src/main/java/com/devpredator/tiendamusicalweb/controllers/HomeController.java
@@ -3,9 +3,11 @@
  */
 package com.devpredator.tiendamusicalweb.controllers;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
@@ -15,6 +17,8 @@ import org.apache.logging.log4j.Logger;
 
 import com.devpredator.tiendamusicalentities.dto.ArtistaAlbumDTO;
 import com.devpredator.tiendamusicalservices.service.HomeService;
+import com.devpredator.tiendamusicalweb.session.SessionBean;
+import com.devpredator.tiendamusicalweb.utils.CommonUtils;
 
 /**
  * @author Diego
@@ -43,6 +47,11 @@ public class HomeController {
 	@ManagedProperty("#{homeServiceImpl}")
 	private HomeService homeServiceImpl;
 	/**
+	 * Objeto que almacena informacion en sesion.
+	 */
+	@ManagedProperty("#{sessionBean}")
+	private SessionBean sessionBean;
+	/**
 	 * Inicializando pantalla.
 	 */
 	@PostConstruct
@@ -64,6 +73,19 @@ public class HomeController {
 			this.artistasAlbumDTO.forEach(artistaAlbumDTO ->{
 				LOGGER.info("Artista: " + artistaAlbumDTO.getArtista().getNombre());
 			});
+		}
+	}
+	/**
+	 * Metodo que permite ver el detalle del album seleccionado por el cliente.
+	 * @param artistaAlbumDTO {@link ArtistaAlbumDTO} objeto con el album seleccionado.
+	 */
+	public void verDetalleAlbum(ArtistaAlbumDTO artistaAlbumDTO) {
+		this.sessionBean.setArtistaAlbumDTO(artistaAlbumDTO);
+		try {
+			CommonUtils.redireccionar("/pages/cliente/detalle.xhtml");
+		} catch (IOException e) {
+			CommonUtils.mostrarMensaje(FacesMessage.SEVERITY_ERROR, "Ups", "Hubo un error de formato en la pagina a ingresar. Favor de contactar con soporte.");
+			e.printStackTrace();
 		}
 	}
 	/**
@@ -101,6 +123,18 @@ public class HomeController {
 	 */
 	public void setHomeServiceImpl(HomeService homeServiceImpl) {
 		this.homeServiceImpl = homeServiceImpl;
+	}
+	/**
+	 * @return the sessionBean
+	 */
+	public SessionBean getSessionBean() {
+		return sessionBean;
+	}
+	/**
+	 * @param sessionBean the sessionBean to set
+	 */
+	public void setSessionBean(SessionBean sessionBean) {
+		this.sessionBean = sessionBean;
 	}
 	
 	
